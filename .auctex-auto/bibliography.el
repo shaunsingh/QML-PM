64ff6a5fcab621d37dd9b7f9768edbbaa0d947c3
shaunsingh <shaunsingh0207@gmail.com>
2022-01-23 13:18:24 -0500

    (git) add gitignore

--- .auctex-auto/bibliography.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- "bibliography"
- (lambda ()
-   (LaTeX-add-bibitems
-    "Emily:2020"
-    "IBM:2021"))
- :bibtex)
-
--- .auctex-auto/paper.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- "paper"
- (lambda ()
-   (LaTeX-add-bibitems
-    "2011"
-    "Non-federal"))
- :bibtex)
-
