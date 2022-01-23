a02e35db70ff90dd04ffddfebfb686b05bd9316c
shaunsingh <shaunsingh0207@gmail.com>
2022-01-23 13:17:26 -0500

    (paper/bibliography) setup citation processor, adjust formatting

---
+++ .auctex-auto/bibliography.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook
+ "bibliography"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Emily:2020"
+    "IBM:2021"))
+ :bibtex)
+
---
+++ .auctex-auto/paper.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook
+ "paper"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "2011"
+    "Non-federal"))
+ :bibtex)
+
